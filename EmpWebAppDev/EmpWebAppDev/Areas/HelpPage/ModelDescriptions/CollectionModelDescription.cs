7772b66e7405a3c3180e9947e5bb4d0f0eb7f450
Leonard Hayes <lhayes32@students.kennesaw.edu>
2019-01-03 00:25:42 -0500

    Need to restart due to microsoft update

--- EmpWebAppDev/EmpWebAppDev/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace EmpWebAppDev.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
