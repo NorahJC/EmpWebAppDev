89c7212fc13ae00baee77cbc440e75c0ab704c29
Leonard Hayes <lhayes32@students.kennesaw.edu>
2019-01-05 19:10:27 -0500

    Library Services Update 1

---
+++ Library/LibraryServices/LibraryAssetService.cs
@@ -0,0 +1,68 @@
+﻿using LibraryData;
+using LibraryData.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LibraryServices
+{
+    public class LibraryAssetService : ILibraryAsset
+    {
+        private LibraryContext _context;
+
+        public LibraryAssetService(LibraryContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(LibraryAsset newAsset)
+        {
+            _context.Add(newAsset);
+            _context.SaveChanges();
+        }
+
+        public IEnumerable<LibraryAsset> GetAll()
+        {
+           return _context.LibraryAssets
+                .Include(asset => asset.Status)
+                .Include(asset => asset.Location);
+        }
+
+        public string GetAuthorOrDirector(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public LibraryAsset GetById(int id)
+        {
+            return GetAll()
+                .FirstOrDefault(asset => asset.Id == id);
+        }
+
+        public LibraryBranch GetCurrentLocation(int id)
+        {
+            return GetById(id).Location;
+        }
+
+        public string GetDeweyIndex(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetIsbn(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetTitle(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetType(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
