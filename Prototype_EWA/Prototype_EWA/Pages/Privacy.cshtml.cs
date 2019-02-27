ad45a9ab7bfffc03e79c09a16e407496b4a0c218
lhayes32 <Leo Hayes>
2019-02-27 10:44:15 -0500

    Adding prototype

---
+++ Prototype_EWA/Prototype_EWA/Pages/Privacy.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Prototype_EWA.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
