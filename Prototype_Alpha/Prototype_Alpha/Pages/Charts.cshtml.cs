4ff80aaa805926fc27661922adfd99b4c5d617da
Hayes, Leonard <Leonard.Hayes@gtri.gatech.edu>
2019-02-28 10:10:49 -0500

    New Prototype with Chart tab

---
+++ Prototype_Alpha/Prototype_Alpha/Pages/Charts.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Prototype_Alpha.Pages
+{
+    public class ChartsModel : PageModel
+    {
+        [BindProperty]
+        public string Name { get; set; }
+
+        [BindProperty]
+        public string Email { get; set; }
+
+        public void OnGet()
+        {
+        }
+        public void OnPost()
+        {
+            ViewData["confirmation"] = $"{Name}, information will be sent to {Email}";
+        }
+    }
+}
