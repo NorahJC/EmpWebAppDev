717f1e5f89354eb4c041d70bb051f718ebc3a35b
Hayes, Leonard <Leonard.Hayes@gtri.gatech.edu>
2019-02-28 11:24:27 -0500

    Added random chart

--- Prototype_Alpha/Prototype_Alpha/Pages/Charts.cshtml.cs
+++ Prototype_Alpha/Prototype_Alpha/Pages/Charts.cshtml.cs
@@ -9,18 +9,6 @@ namespace Prototype_Alpha.Pages
 {
     public class ChartsModel : PageModel
     {
-        [BindProperty]
-        public string Name { get; set; }
 
-        [BindProperty]
-        public string Email { get; set; }
-
-        public void OnGet()
-        {
-        }
-        public void OnPost()
-        {
-            ViewData["confirmation"] = $"{Name}, information will be sent to {Email}";
-        }
     }
 }
